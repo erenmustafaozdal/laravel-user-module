6b2658a5c9a30539c13e026584cf9e93cb9b7bc8
erenMustafaOzdal <eren.060737@gmail.com>
2016-06-11 14:21:52 +0000

    user

--- src/Http/Controllers/UserApiController.php
+++ src/Http/Controllers/UserApiController.php
@@ -3,6 +3,7 @@
 namespace ErenMustafaOzdal\LaravelUserModule\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 use App\Http\Requests;
 use Yajra\Datatables\Facades\Datatables;
